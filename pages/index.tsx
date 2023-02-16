21339d09bfce26f66c71883ccb0cb13431ab876f
Nghi1100 <vovannghi20@gmail.com>
2023-02-16 19:28:23 +0700

    change text

--- pages/index.tsx
+++ pages/index.tsx
@@ -48,7 +48,7 @@ export default function index() {
                   href="/about"
                   className="text-base font-semibold leading-7 text-gray-900"
                 >
-                  Learn more <span aria-hidden="true">→</span>
+                  {t("readMore")} <span aria-hidden="true">→</span>
                 </a>
               }
             />
